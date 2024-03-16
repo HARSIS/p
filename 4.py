5e852e9620f40bdf9d07d3ae897471c24c7f695f
HARSIS <91324320+HARSIS@users.noreply.github.com>
2024-03-16 13:01:34 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -2,6 +2,7 @@ f = open("products.csv", encoding="UTF-8").read().split("\n") #чтение фа
 f[0] = f[0] + ";promocode"
 for i in range(1, len(f)): #добовляю столбец
     a = f[i].split(';')
-    f[i] = f[i] + ";" + a[1][:2] + a[2][:2] + a[1][-1] + a[1][-2] + a[2][4] + a[2][3]
+    f[i] = f[i] + ";" + a[1][:2] + a[2][:2] + a[1][-1] + a[1][-2] + a[2][4] + a[2][3]#собираю строчку
+    f[i] = f[i].upper()#делаю все буквы загалные
 f = "\n".join(f)
 open("product_promo.csv", mode="w", encoding="UTF-8").write(f) #сахраняю результат в новом файле
