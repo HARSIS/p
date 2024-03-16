41386c1626ea564ef30bde399c499096d58499fa
HARSIS <91324320+HARSIS@users.noreply.github.com>
2024-03-16 12:23:32 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,7 +1,7 @@
-f = open("products.csv", encoding="UTF-8").read().split("\n")
+f = open("products.csv", encoding="UTF-8").read().split("\n") #чтение файла
 f[0] = f[0] + ";promocode"
-for i in range(1, len(f)):
+for i in range(1, len(f)): #добовляю столбец
     a = f[i].split(';')
     f[i] = f[i] + ";" + a[1][:2] + a[2][:2] + a[1][-1] + a[1][-2] + a[2][4] + a[2][3]
 f = "\n".join(f)
-open("product_promo.csv", mode="w", encoding="UTF-8").write(f)
+open("product_promo.csv", mode="w", encoding="UTF-8").write(f) #сахраняю результат в новом файле
