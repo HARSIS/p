b8cf878398ce643dbadd0bc8e99a5cb96a4bc724
HARSIS <91324320+HARSIS@users.noreply.github.com>
2024-03-16 12:11:13 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
-f = open("products.csv", encoding="UTF-8").read().split("\n")
+f = open("products.csv", encoding="UTF-8").read().split("\n") #чтение файла
 f[0] = f[0] + ";total"
 for i in range(1, len(f)):
     a = f[i].split(';')
-    f[i] = f[i] + ";" + str(float(a[3]) * float(a[4]))
+    f[i] = f[i] + ";" + str(float(a[3]) * float(a[4])) #добовление столбца
 f = "\n".join(f)
-open("products_new.csv", mode="w", encoding="UTF-8").write(f)
+open("products_new.csv", mode="w", encoding="UTF-8").write(f) # запись файла
